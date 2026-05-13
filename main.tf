e2d11d4598a290c56a5e93d243c38663b8bd7b32
Hrithik  Gaikwad <hrithikgaikwad@Aryans-MacBook-Air.local>
2026-05-14 01:27:54 +0530

    State Locking in S3+DynamoDB

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "WebServer"
+  }
+  
+}
